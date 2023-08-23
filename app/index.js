7b8dbfa6e3735b95b44bc852af7da4ff6100612e
nojram00 <113487670+nojram00@users.noreply.github.com>
2023-08-23 17:41:47 +0800

    s

--- app/index.js
+++ app/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const connection = require('./config/mysql.config')
 const { Sample } = require('./model/sample.model')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
 const app = express()
 const port = 8082
+
+ra = require('crypto').randomBytes(64).toString('hex')
+console.log(ra)
+
+dotenv.config();
+
+process.env.TOKEN_SECRET;
+
+
 
 app.get('/', (req, res) => {
     // sample = new Sample()
@@ -28,6 +39,34 @@ app.get('/employees', (req, res) => {
         res.json(results).status(200)
     })
 })
+
+
+app.post('/createUser', (req, res) => {
+    const token = generateAccessToken({username: req.body.username})
+    res.json(token).status(200)
+})
+
+app.post('/login', authenticate, (req, res) => {
+    const token = generateAccessToken({username: req.body.username})
+    res.json(token).status(200)
+})
+
+function authenticate(res, req, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token==null) return res.sample(401)
+
+    jwt.verify(token, process.env.TOKEN_SECRET.toString(), (err, user) => {
+        console.log(err)
+
+        if (err) return res.status(403)
+
+        req.user = user
+
+        next()
+    })
+}
 
 app.listen(port, () => [
     console.log(`Listen on ${port}`)
--- app/model/base.model.js
+++ app/model/base.model.js
@@ -8,8 +8,27 @@ class Model {
         return this.table
     }
 
-    select(){
-        this.query = `SELECT * FROM ${this.table}`
+    select(cols = '*'){
+        this.query = `SELECT ${cols} FROM ${this.table}`
+        return this
+    }
+
+    /**
+     *
+     * @param table {string} the table you want to join
+     * @param args {string} arguments after 'ON' clause. (Ex: "table1.id = table2.id")
+     */
+    join(table,args){
+        this.query += ` JOIN ${table} ON ${args}`
+        return this
+    }
+
+    /**
+     *
+     * @param args {string} arguments after 'WHERE' clause. (Ex: "id = 4", "BETWEEN 4 AND 5", "LIKE b%", etc.)
+     */
+    where(args){
+        this.query += ` WHERE ${args}`
         return this
     }
 
